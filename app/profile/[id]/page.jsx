03930eb85749a1ac8466c64d13a49fc9cd9d508b
Ojas Elawadhi <ojaselawadhi1@gmail.com>
2023-06-28 01:29:46 +0530

    first commit

---
+++ app/profile/[id]/page.jsx
@@ -0,0 +1,40 @@
+"use client"
+
+import { useState, useEffect } from 'react'
+import { useSession } from 'next-auth/react'
+import { useSearchParams } from 'next/navigation'
+
+import Profile from '@components/profile';
+
+const UserProfile = ({params}) => {
+  
+  const { data: session } = useSession();
+  const searchParams = useSearchParams();
+  const userName = searchParams.get("name");
+  const [userPosts, setUserPosts] = useState([]);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const response = await fetch(`/api/users/${params?.id}/posts`)
+      const data = await response.json();
+
+      setUserPosts(data);
+    }
+
+    if(params?.id)
+        fetchPosts(); 
+
+  }, [params?.id])
+      
+  
+
+  return (
+    <Profile
+        name={userName}
+        desc="Welcome to your personalized profile page"
+        data={userPosts}
+    />
+  )
+}
+
+export default UserProfile
